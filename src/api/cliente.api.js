a340c79c974a2f00f5a02efe83155430a29e4a1e
Mario Quevedo <opnfuso@gmail.com>
2022-10-11 19:33:28 -0500

    cliente api and dotenv

--- src/api/cliente.api.js
+++ src/api/cliente.api.js
@@ -1,6 +1,16 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000";
-
 export const getClientesRequest = async () =>
-  await axios.get(`${BASE_URL}/clientes`);
+  await axios.get(`${process.env.BASE_URL}/clientes`);
+
+export const getClienteRequest = async (id) =>
+  await axios.get(`${process.env.BASE_URL}/clientes/${id}`);
+
+export const createClienteRequest = async (id, cliente) =>
+  await axios.post(`${process.env.BASE_URL}/clientes`, cliente);
+
+export const updateClienteRequest = async (id, cliente) =>
+  await axios.patch(`${process.env.BASE_URL}/clientes/${id}`, cliente);
+
+export const deleteClienteRequeste = async (id) =>
+  await axios.delete(`${process.env.BASE_URL}/clientes/${id}`);
