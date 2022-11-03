ea5c4314824a2e0bae263716d422c4fa70f5b77b
alexis8500 <saul.perez.dev@gmail.com>
2022-11-03 10:02:36 -0600

    fecha nacimiento cliente error

--- src/schemas/clientes/create.schema.js
+++ src/schemas/clientes/create.schema.js
@@ -9,7 +9,13 @@ export const createClienteSchema = object({
     .matches(/^[A-Za-z\s]*$/)
     .required("los apellidos son requeridos"),
   domicilio: string().required("el domicilio es requerido"),
-  fecha_nacimiento: date().required("la fecha de nacimiento es requerida"),
+  fecha_nacimiento: date()
+  .max(
+    //older than 9 years
+    new Date(new Date().setFullYear(new Date().getFullYear() - 9)),
+    "Debe ser mayor de 9 años"
+  )
+  .required("la fecha de nacimiento es requerida"),
   telefono: string()
     .phone("+52", true, "el numero de telefono debe de ser correcto")
     .required("el telefono es requerido"),
