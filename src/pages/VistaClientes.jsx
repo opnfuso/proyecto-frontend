a53684708c2294d0dd6ee9346f5d8d833c81f92b
Mario Quevedo <opnfuso@gmail.com>
2022-11-11 16:26:20 -0600

    search bar

--- src/pages/VistaClientes.jsx
+++ src/pages/VistaClientes.jsx
@@ -6,13 +6,38 @@ import { Link } from "react-router-dom";
 
 const VistaClientes = () => {
   const [clientes, setClientes] = useState([]);
+  const [search, setSearch] = useState([]);
 
   const loadClientes = async () => {
     try {
       const response = await getClientesRequest();
       setClientes(response.data);
+      setSearch(response.data);
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const handleSearchChange = (e) => {
+    const s = e.target.value;
+
+    if (s.length === 0) {
+      setSearch(clientes);
+    } else {
+      let ser = [];
+
+      clientes.forEach((cli) => {
+        const str =
+          `${cli.nombres} ${cli.apellidos} ${cli.email} ${cli.telefono}`.toLowerCase();
+
+        if (str.includes(s.toLowerCase())) {
+          ser.push(cli);
+        }
+      });
+
+      setSearch(ser);
+
+      console.log(search);
     }
   };
 
@@ -32,6 +57,19 @@ const VistaClientes = () => {
                   Clientes
                 </span>
               </div>
+              <form className="d-flex" role="search">
+                <input
+                  onChange={(e) => {
+                    handleSearchChange(e);
+                  }}
+                  style={{ width: 500 }}
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Buscar"
+                  aria-label="Search"
+                />
+              </form>
+
               <Link
                 to="/new/cliente"
                 className="btn btn-primary btn-agregar-usuario"
@@ -50,7 +88,7 @@ const VistaClientes = () => {
                   role="grid"
                   aria-describedby="dataTable_info"
                 >
-                  <ListClientes clientes={clientes} key={clientes.id} />
+                  <ListClientes clientes={search} key={clientes.id} />
                 </div>
               </div>
             </div>
