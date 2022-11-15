725844ae228ad01689053e8a54ce8abd0c3a6ddf
Mario Quevedo <opnfuso@gmail.com>
2022-11-14 19:03:05 -0600

    delete comments in refacciones

--- src/api/refacciones.api.js
+++ src/api/refacciones.api.js
@@ -1,20 +1,6 @@
 import axios from "axios";
 
 export const getRefaccionesRequest = async (query) => {
-  // const options = {
-  //   headers: {
-  //     "content-type": "application/json",
-  //     "X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_KEY,
-  //     "X-RapidAPI-Host": process.env.REACT_APP_RAPIDAPI_HOST,
-  //   },
-  // };
-
-  // const data = {
-  //   q: query,
-  //   location_name: "Mexico",
-  //   location_parameters_auto: "true",
-  // };
-
   const options = {
     method: "GET",
     url: process.env.REACT_APP_RAPIDAPI_URL,
@@ -30,6 +16,4 @@ export const getRefaccionesRequest = async (query) => {
   };
 
   return await axios.request(options);
-
-  // return await axios.get(process.env.REACT_APP_RAPIDAPI_URL, data, options);
 };
